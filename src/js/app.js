24248be2f495a94aaee9b03a28f280e17f800b75
Leon Chen <bigfact@foxmail.com>
2015-12-12 04:21:58 +0800

    所有页面原型完成，待数据绑定。

--- src/js/app.js
+++ src/js/app.js
@@ -17,17 +17,35 @@ xfapp.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: "news.html",
 			controller: "newsContr"
         })
+        // 新闻详情
+        .state('newsdetail', {
+            url: "/newsdetail",
+            templateUrl: "newsdetail.html",
+            controller: "newsdetailContr"
+        })
 		// 教练风采
         .state('teachers', {
             url: "/teachers",
             templateUrl: "teachers.html",
 			controller: "teachersContr"
         })
+        // 教练介绍
+        .state('teacherinfo', {
+            url: "/teacherinfo",
+            templateUrl: "teacherinfo.html",
+            controller: "teacherinfoContr"
+        })
 		// 兴甫风采
         .state('xingfufengcai', {
             url: "/xingfufengcai",
             templateUrl: "xingfufengcai.html",
 			controller: "xingfufengcaiContr"
+        })
+        // 兴甫风采详情
+        .state('xingfufengcaidetail', {
+            url: "/xingfufengcaidetail",
+            templateUrl: "xingfufengcaidetail.html",
+            controller: "xingfufengcaidetailContr"
         })
 		// 在线报名
         .state('regist', {
